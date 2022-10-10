f0509f0135363cea1415b96dbad7a6c92df46734
Swapnil639 <swapnilpatil631997@gmail.com>
2022-10-10 19:32:13 +0530

    Added :Updated salary data using prepared statement

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -9,14 +9,21 @@ public class EmployeePayrollService {
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
         String userName = "root";
         String password = "Swapnil123";
+        double basicPay = 5000000.0;
+        int id = 4;
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded!");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection done!!");
+            String query = "UPDATE employee_payroll SET basic_Pay=? WHERE id=?";
+            PreparedStatement prestatement = connection.prepareStatement(query);
+            prestatement.setDouble(1, basicPay);
+            prestatement.setInt(2, id);
+            prestatement.executeUpdate();
+
             Statement statement = connection.createStatement();
-            statement.execute("update employee_payroll set basic_pay = 3000000 where name = 'Swapnil'");
             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll_details");
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("name") + " | " + resultSet.getString("gender") + " | " + resultSet.getString("phone_number") + " | " + resultSet.getString("address") + " | " + resultSet.getString("department") + " | " + resultSet.getDouble("basicPay") + " | " + resultSet.getDouble("deductions") + " | " + resultSet.getDouble("taxablePay") + " | " + resultSet.getDouble("netPay") + " | " + resultSet.getDouble("incomeTax") + " | " + resultSet.getDate("start"));
