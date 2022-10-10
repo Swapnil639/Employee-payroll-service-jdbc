b12e34355b65aecfae193fba810cf84acc726f88
Swapnil639 <swapnilpatil631997@gmail.com>
2022-10-10 19:04:38 +0530

    Added :Retrieved data from database

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,8 +1,6 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class EmployeePayrollService {
     public static void main(String[] args) throws SQLException {
@@ -17,7 +15,12 @@ public class EmployeePayrollService {
             System.out.println("Driver loaded!");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection done!!");
-        } catch (Exception  e) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM payroll_service.employee_details");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("name") + " | " + resultSet.getString("gender") + " | " + resultSet.getString("phone_number") + " | " + resultSet.getString("address") + " | " + resultSet.getString("department") + " | " + resultSet.getDouble("basicPay") + " | " + resultSet.getDouble("deductions") + " | " + resultSet.getDouble("taxablePay") + " | " + resultSet.getDouble("netPay") + " | " + resultSet.getDouble("incomeTax") + " | " + resultSet.getDate("start"));
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             connection.close();
