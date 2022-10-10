2ec839bf4f8fb258d541fb8be56b8629719c6e5d
Swapnil639 <swapnilpatil631997@gmail.com>
2022-10-10 15:15:17 +0530

    Added :Connected database to java program successfully

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,7 +1,26 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollService {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Payroll service");
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Status of connecting database to java code: ");
+
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+        String userName = "root";
+        String password = "Swapnil123";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection done!!");
+        } catch (Exception  e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 }
