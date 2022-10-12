14789c398e1606453af2da3f8e47e0c9bd57e162
Swapnil639 <swapnilpatil631997@gmail.com>
2022-10-12 18:55:19 +0530

    Added :Added new employee details to table

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -5,7 +5,6 @@ import java.sql.*;
 public class EmployeePayrollService {
     public static void main(String[] args) throws SQLException {
         System.out.println("Status of connecting database to java code: ");
-
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
         String userName = "root";
         String password = "Swapnil123";
@@ -17,11 +16,11 @@ public class EmployeePayrollService {
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection done!!");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT SUM(basic_pay), AVG(basic_pay), MAX(basic_pay), MIN(basic_pay) FROM employee_payroll_details WHERE gender = 'M' GROUP BY gender ");
+            statement.execute("insert into employee_payroll(name,department,gender,basic_pay,deduction,taxable_pay,income_tax,net_pay,start)values('Swapnil','marketing','M',3000000.00,1000000.00,2000000.00,5000000.00,1500000.00,'2018-01-03')");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll_details");
             while (resultSet.next()) {
-                System.out.println("Sum of all basic salary " + resultSet.getDouble("SUM(basic_pay)") + "\n" + "Average of all basic salary " + resultSet.getDouble("AVG(basic_pay)") + "\n" + "Min of all basic salary " + resultSet.getDouble("MIN(basic_pay)") + "\n" + "MAX of all basic salary " + resultSet.getDouble("MAX(basic_pay)"));
+                System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("name") + " | " + resultSet.getString("gender") + " | " + resultSet.getString("phone_number") + " | " + resultSet.getString("address") + " | " + resultSet.getString("department") + " | " + resultSet.getDouble("basic_pay") + " | " + resultSet.getDouble("deductions") + " | " + resultSet.getDouble("taxable_pay") + " | " + resultSet.getDouble("net_pay") + " | " + resultSet.getDouble("income_tax") + " | " + resultSet.getDate("start"));
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
